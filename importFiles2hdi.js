ca20210bfedcc66fd377f67edfaf6db24559d74a
dantecsm <4spadenil@gmail.com>
2025-08-10 23:45:04 +0800

    feat: 添加 yarn hdi 指令，可自动导入修改后的 MES 文件到 hdi

---
+++ importFiles2hdi.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const path = require('path')
+const FatImage = require('./replaceHdiFile')
+
+const hdiFile = "J:/PC98 Files/Bay City Elegy_CN.hdi"
+const bootSectorOffset = 0x9400
+const newFilesDir = 'RB_CN_MES'
+const targetPathPrefix = '//YOKOHAMA/RB_MES/'
+// "//YOKOHAMA/RB_MES/MSG01.MES"
+// "./RB_CN_MES/MSG01.MES"
+
+const newFiles = fs.readdirSync(newFilesDir)
+const img = new FatImage(hdiFile, bootSectorOffset)
+for (const newFile of newFiles) {
+    const newFilePath = path.join(newFilesDir, newFile)
+    const newFileBuffer = fs.readFileSync(newFilePath)
+    const targetPath = `${targetPathPrefix}${newFile}`
+    img.replaceFile(targetPath, newFileBuffer)
+}
+img.close()
--- test.js
+++ test.js
@@ -7,34 +7,34 @@ const gtOutputDir = 'gt_output'
 const nameSet = new Set()
 
 // 读取 gt_input 目录下所有文件
-const inputFiles = fs.readdirSync(gtInputDir)
+// const inputFiles = fs.readdirSync(gtInputDir)
 
-inputFiles.forEach(file => {
-    const inputPath = path.join(gtInputDir, file)
-    const outputPath = path.join(gtOutputDir, file)
-    if (!fs.existsSync(outputPath)) {
-        console.warn(`未找到输出文件: ${outputPath}`)
-        return
-    }
-    // 读取输入和输出文件
-    const inputArr = JSON.parse(fs.readFileSync(inputPath, 'utf8'))
-    const outputArr = JSON.parse(fs.readFileSync(outputPath, 'utf8'))
+// inputFiles.forEach(file => {
+//     const inputPath = path.join(gtInputDir, file)
+//     const outputPath = path.join(gtOutputDir, file)
+//     if (!fs.existsSync(outputPath)) {
+//         console.warn(`未找到输出文件: ${outputPath}`)
+//         return
+//     }
+//     // 读取输入和输出文件
+//     const inputArr = JSON.parse(fs.readFileSync(inputPath, 'utf8'))
+//     const outputArr = JSON.parse(fs.readFileSync(outputPath, 'utf8'))
 
-    // 遍历输出数组，为每个成员添加原名和原文字段
-    for (let i = 0; i < outputArr.length; i++) {
-        const a = inputArr[i]
-        const b = outputArr[i]
-        // 处理 a 可能没有 name 字段的情况
-        b['原名'] = a.hasOwnProperty('name') ? a.name : ''
-        b['原文'] = a.message
+//     // 遍历输出数组，为每个成员添加原名和原文字段
+//     for (let i = 0; i < outputArr.length; i++) {
+//         const a = inputArr[i]
+//         const b = outputArr[i]
+//         // 处理 a 可能没有 name 字段的情况
+//         b['原名'] = a.hasOwnProperty('name') ? a.name : ''
+//         b['原文'] = a.message
 
-        if (a.name) nameSet.add(a.name)
-    }
+//         if (a.name) nameSet.add(a.name)
+//     }
 
-    // 写回输出文件
-    fs.writeFileSync(outputPath, JSON.stringify(outputArr, null, 2), 'utf8')
-    console.log(`已处理: ${file}`)
-})
+//     // 写回输出文件
+//     fs.writeFileSync(outputPath, JSON.stringify(outputArr, null, 2), 'utf8')
+//     console.log(`已处理: ${file}`)
+// })
 // console.log(Array.from(nameSet))
 
 // const names = ['愛音', '山崎', '東山', '菅原', ' 悠 ', '(声)', '美夏', ' 客 ', '神緒', 'レポーターＢ', 'レポーターＡ', 'レポーターＣ',
@@ -67,7 +67,8 @@ files.forEach(file => {
 
     const jpStat = fs.statSync(jpFile)
     const cnStat = fs.statSync(cnFile)
-    if (jpStat.size !== cnStat.size) {
-        console.log(`文件大小不一致: ${file}，日文文件大小: ${jpStat.size}，中文文件大小: ${cnStat.size}`)
+    if (jpStat.size < cnStat.size) {
+        const diff = cnStat.size - jpStat.size
+        console.log(`文件大小不一致: ${file}，中文文件比日文文件大 ${diff} 字节`)
     }
 })
